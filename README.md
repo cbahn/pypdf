8de8aca06e39d657f130ec6d021a29eff4482dd1
Carl <junco3@gmail.com>
2025-03-11 11:30:36 -0500

    Added some CLI utilities

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pypdf
 A small utility for placing images in pdfs to print on Avery 94237 sticker sheets
+
+
+## Todo
+[] It should detect if images are in portrait or landscape and rotate them if needed
+[] add CLI features like using -o to specify an output file name
--- to_pdf.py
+++ to_pdf.py
@@ -1,5 +1,7 @@
 from fpdf import FPDF
+import argparse
 import os
+import sys
 
 
 #Original Values
@@ -21,50 +23,65 @@ def add_image(pdf_object, filename, location):
     x = x_values[location]
     pdf_object.image(filename, x, y, 50, 75)
 
+def main():
+    parser = argparse.ArgumentParser(description="Sticker sheet image templating utility")
+    parser.add_argument("-o", "--output", type=str, help="Output file name", required=False)
+    parser.add_argument("-f", "--folder", type=str, help="Specify a folder with images")
+    args = parser.parse_args()
+
+    
+    # Create the pdf and initialize a page
+    pdf = FPDF('L','mm','Letter')
+    pdf.set_font('Arial','',16)
+    pdf.add_page()
+
+    # I'm not totally sure, but it seems the PDF prints differently based on the leftmost and topmost 
+    # item placed on the page. Putting this "_" in the upper left seems to fix it.
+    pdf.text(0,0,"_")
 
 
-# Create the pdf and initialize a page
-pdf = FPDF('L','mm','Letter')
-pdf.set_font('Arial','',16)
-pdf.add_page()
-
-# I'm not totally sure, but it seems the PDF prints differently based on the leftmost and topmost 
-# item placed on the page. Putting this "_" in the upper left seems to fix it.
-pdf.text(0,0,"_")
+    # This code is for putting coordinates all over the page for troubleshooting purposes
+    # for x in range (0,300,23):
+    #     for y in range(0,300,8):
+    #         pdf.text(x,y,f"{x}x{y}")
 
 
-# This code is for putting coordinates all over the page for troubleshooting purposes
-# for x in range (0,300,23):
-#     for y in range(0,300,8):
-#         pdf.text(x,y,f"{x}x{y}")
+    """
+    Page Layout is:
+    0 1 2 3
+    4 5 6 7
+
+    Images should be 510x770
+
+    When the paper is put into the paper tray, it should be placed sticker-side down.
+    The upper left corner of the pdf will be printed to the corner of the page deepest in the printer on the left side.
+    """
+
+    if not args.folder:
+        print("You must either specify a file or a folder as input")
+        sys.exit(1)
+
+    folder_path = args.folder
+    pos_iter = 0
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        # Check if it's a file and not a directory
+        if os.path.isfile(file_path):
+            if pos_iter > 7:
+                pdf.add_page()
+                pdf.text(0,0,"_")
+                pos_iter = 0
+
+            add_image(pdf,file_path, pos_iter)
+            pos_iter += 1
+
+    output_file = "output.pdf"
+    if args.output:
+        output_file = args.output
+
+    pdf.output(output_file, "F")
 
 
-"""
-Page Layout is:
-0 1 2 3
-4 5 6 7
-
-Images should be 510x770
-
-When the paper is put into the paper tray, it should be placed sticker-side down.
-The upper left corner of the pdf will be printed to the corner of the page deepest in the printer on the left side.
-"""
-
-folder_path = "fall24"
-pos_iter = 0
-
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    # Check if it's a file and not a directory
-    if os.path.isfile(file_path):
-        if pos_iter > 7:
-            pdf.add_page()
-            pdf.text(0,0,"_")
-            pos_iter = 0
-
-        add_image(pdf,file_path, pos_iter)
-        pos_iter += 1
-        #print(file_path)
-
-
-pdf.output("yourfile.pdf", "F")
+if __name__ == "__main__":
+    main()
